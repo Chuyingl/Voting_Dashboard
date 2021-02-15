7319ede60a160387aac50faab245244a9b0d978d
chuyi <chuyingl@umich.edu>
2021-02-14 21:08:41 -0800

    initiate a db access

---
+++ rest-api/dal/ng-question-dal.js
@@ -0,0 +1,34 @@
+const mongojs =require("mongojs");
+const db =mongojs("technology-voting-db", ["ngQuestions", "angularVotes"]);
+
+class NgQuestionDal {
+    constructor(){
+
+    }
+
+    getAllAngularQuestions (){
+      return new Promise ((resolve, reject) =>{
+          db.ngQuestions.find((err, docs) =>{
+              if(err){
+                  reject(err);
+                  return;
+              }
+              resolve(docs);
+          })
+      })  
+    }
+
+    newVote(vote) {
+        return new Promise ((resolve, reject) => {
+            db.angularVotes.insert(vote, (err,doc) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve(doc);
+            })
+        })
+    }
+}
+
+module.exports =new NgQuestionDal();
